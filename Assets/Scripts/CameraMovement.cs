861abb0454ee50e0ca1589e8d01d6b8b3e1c0649
Iker <ikernoya4@gmail.com>
2020-09-16 15:07:01 -0300

    Created bar to show map progression #92

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,18 +8,25 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] GameObject crown;
     [SerializeField] float speed;
+    [SerializeField] float offset;
     float lastY;
 
     Vector3 direction;
+    private void Start()
+    {
+        if (crown != null)
+        {
+            direction = crown.transform.position - transform.position;
+            direction = new Vector3(0, direction.y, 0);
+        }
+    }
     void Update()
     {
         if(crown!=null)
         {
-            direction = crown.transform.position - transform.position;
             lastY = crown.transform.position.y;
         }
-        direction = new Vector3(0, direction.y, 0);
-        if (transform.position.y < lastY) 
+        if (transform.position.y < lastY-offset) 
             transform.position += direction * speed * Time.deltaTime;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Image map_L;
+    [SerializeField] Image map_R;
+    [SerializeField] Transform target;
+    [SerializeField] float offset;
+
+    Camera cam;
+    float distanceToGoal;
     void Start()
     {
         PlayerController.endGame += EndGame;
+        map_L.fillAmount = 0f;
+        map_R.fillAmount = 0f;
+        cam = Camera.main;
+        distanceToGoal = target.position.y - cam.transform.position.y - offset;
     }
     void Update()
     {
-        
+        float currentCameraDistance = target.position.y - cam.transform.position.y - offset;
+        float progress = 1f - (currentCameraDistance / distanceToGoal);
+        if(cam.transform.position.y >= target.position.y - offset)
+        {
+            map_L.fillAmount = 1f;
+            map_R.fillAmount = 1f;
+        }
+        map_L.fillAmount = progress;
+        map_R.fillAmount = progress;
     }
     void EndGame()
     {
