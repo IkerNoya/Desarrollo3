87a63e6b751b127084814b6058b8811a0a3ffe76
Iker <ikernoya4@gmail.com>
2020-09-17 10:21:08 -0300

    Changed player 2 controls

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Image map_L;
-    [SerializeField] Image map_R;
+    [SerializeField] Image map;
     [SerializeField] Transform target;
     [SerializeField] float offset;
 
@@ -14,22 +13,23 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         PlayerController.endGame += EndGame;
-        map_L.fillAmount = 0f;
-        map_R.fillAmount = 0f;
+        map.fillAmount = 0f;
         cam = Camera.main;
-        distanceToGoal = target.position.y - cam.transform.position.y - offset;
+        if(target!=null)
+            distanceToGoal = target.position.y - cam.transform.position.y - offset;
     }
     void Update()
     {
-        float currentCameraDistance = target.position.y - cam.transform.position.y - offset;
-        float progress = 1f - (currentCameraDistance / distanceToGoal);
-        if(cam.transform.position.y >= target.position.y - offset)
+        if (target != null)
         {
-            map_L.fillAmount = 1f;
-            map_R.fillAmount = 1f;
+            float currentCameraDistance = target.position.y - cam.transform.position.y - offset;
+            float progress = 1f - (currentCameraDistance / distanceToGoal);
+            if (cam.transform.position.y >= target.position.y - offset)
+            {
+                map.fillAmount = 1f;
+            }
+            map.fillAmount = progress;
         }
-        map_L.fillAmount = progress;
-        map_R.fillAmount = progress;
     }
     void EndGame()
     {
