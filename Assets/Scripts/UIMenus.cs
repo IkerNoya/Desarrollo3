507e470586798795dc54bb185efbd75b053a3361
Iker <ikernoya4@gmail.com>
2020-09-29 17:33:15 -0300

    Completed options screen #124

--- Assets/Scripts/UIMenus.cs
+++ Assets/Scripts/UIMenus.cs
@@ -5,11 +5,19 @@ using UnityEngine.EventSystems;
 public class UIMenus : MonoBehaviour
 {
     [SerializeField] GameObject playButton;
-
+    [SerializeField] GameObject OptionsScreen;
     void Update()
     {
         if (EventSystem.current.currentSelectedGameObject != null)
             return;
         EventSystem.current.SetSelectedGameObject(playButton);
+    }
+    public void OnClickOptions()
+    {
+        OptionsScreen.SetActive(true);
+    }
+    public void OnClickBack(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
     }
 }
