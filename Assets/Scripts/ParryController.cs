9427f42a407415aa88f99850817a4044a654a5a0
Iker <ikernoya4@gmail.com>
2020-10-22 12:25:54 -0300

    Renamed parry script

---
+++ Assets/Scripts/ParryController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryController : MonoBehaviour
+{
+    public static event Action<ParryController> parryEffect;
+    
+}
