e2114ce1bafc834dbd442bfa6d229426fe23c496
Iker <ikernoya4@gmail.com>
2020-10-27 22:31:16 -0300

    Removed collision between players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    #region Variables
+    #region Variables1
     [SerializeField] float speed;
     [SerializeField] float wallStickiness;
     [SerializeField] float jumpForce;
