05e23f6d42eada8868f57305a8ee5d9c09ef81c1
Iker <ikernoya4@gmail.com>
2020-11-17 11:53:53 -0300

    Completed Controls scene

---
+++ Assets/Scripts/UIControls.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class UIControls : MonoBehaviour
+{
+
+    [SerializeField] GameObject JoystickButton;
+    [SerializeField] GameObject Joystick;
+    [SerializeField] GameObject Keyboard;
+
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != null)
+            return;
+        if(JoystickButton != null)
+            EventSystem.current.SetSelectedGameObject(JoystickButton);
+    }
+
+    public void OnClickJoystick()
+    {
+        if (Joystick != null)
+        {
+            if (!Joystick.activeSelf)
+            {
+                Joystick.SetActive(true);
+                if (Keyboard != null)
+                Keyboard.SetActive(false);
+            }
+            else
+                Joystick.SetActive(false);
+        }
+    }
+
+    public void OnClickKeyboard()
+    {
+        if (Keyboard != null)
+        {
+            if (!Keyboard.activeSelf)
+            {
+                Keyboard.SetActive(true);
+                if (Joystick != null)
+                    Joystick.SetActive(false);
+            }
+            else
+                Joystick.SetActive(false);
+        }
+    }
+
+}
