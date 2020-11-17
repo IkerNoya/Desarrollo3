e880f2786057cb49405c3d41d0d0d787a9f6005e
Iker <ikernoya4@gmail.com>
2020-11-17 13:07:36 -0300

    music now changes between scenes

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,68 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID ATAQUE1 = 855909871U;
+        static const AkUniqueID ATAQUE2 = 855909868U;
+        static const AkUniqueID ATAQUE3_CRITICO = 1867172427U;
+        static const AkUniqueID CAIDA_PLATAFORMA = 1560490425U;
+        static const AkUniqueID CAPTURA_DEOBJETIVO = 622800721U;
+        static const AkUniqueID CORRIDA = 251416149U;
+        static const AkUniqueID CURSOR_SOBREBOTON = 3050044219U;
+        static const AkUniqueID DAMAGE_JUGADOR1 = 3775992878U;
+        static const AkUniqueID DAMAGE_JUGADOR2 = 3775992877U;
+        static const AkUniqueID DAMAGECRITICO_JUGADOR1 = 3817817795U;
+        static const AkUniqueID DAMAGECRITICO_JUGADOR2 = 3817817792U;
+        static const AkUniqueID DASH = 1942692385U;
+        static const AkUniqueID DESLIZAMIENTO_PARED = 3415848950U;
+        static const AkUniqueID DESPAUSAR = 3406505131U;
+        static const AkUniqueID INICIA_CREDITOS = 4126746240U;
+        static const AkUniqueID INICIA_JUEGO = 1190877053U;
+        static const AkUniqueID INICIA_MENU = 928907744U;
+        static const AkUniqueID INICIA_OPCIONES = 2922467479U;
+        static const AkUniqueID INICIA_SELECCIONDEPANTALLA = 1078793336U;
+        static const AkUniqueID INICIA_SELECCIONDEPERSONAJES = 1725143731U;
+        static const AkUniqueID INICIO = 2658298240U;
+        static const AkUniqueID MENU_PAUSA = 2170009971U;
+        static const AkUniqueID MENU_PLAYERWINS = 34185445U;
+        static const AkUniqueID MUTE = 2974103762U;
+        static const AkUniqueID PARRY = 3076648345U;
+        static const AkUniqueID PARRY_ZOOMIN = 2660185452U;
+        static const AkUniqueID PAUSA = 3092587489U;
+        static const AkUniqueID POPUP_PLAYERWINS = 4149406826U;
+        static const AkUniqueID PRESS_START = 3306940243U;
+        static const AkUniqueID SALTO = 2948438984U;
+        static const AkUniqueID SALTO_PARED = 1230222675U;
+        static const AkUniqueID SUBIR_BAJARFX = 2286640609U;
+        static const AkUniqueID SUBIR_BAJARVOLUMEN = 3489696737U;
+        static const AkUniqueID TIRADO_IDLE = 1171167445U;
+        static const AkUniqueID TOCAR_BOTON = 1436561225U;
+        static const AkUniqueID UNMUTE = 2156697475U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSICAS
+        {
+            static const AkUniqueID GROUP = 3830703178U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MUSICAS
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
