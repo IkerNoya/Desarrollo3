970f953e97e7abec8f01f20fc7c3161e7af13cc2
IkerNoya <ikernoya4@gmail.com>
2021-02-18 19:02:21 -0300

    Created data manager

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    float musicVolume;
+    float sfxVolume;
+
+    public static DataManager instance;
+    public static DataManager Get()
+    {
+       return instance;
+    }
+
+    void Awake()
+    {
+        if(instance!=null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    void Update()
+    {
+        
+    }
+    public float GetMusicVolume()
+    {
+        return musicVolume;
+    }
+    public void SetMusicVolume(float value)
+    {
+        musicVolume = value;
+    }
+    public float GetSFXVolume()
+    {
+        return sfxVolume;
+    }
+    public void SetSFXVolume(float value)
+    {
+        sfxVolume = value;
+    }
+    private void OnApplicationQuit()
+    {
+        Destroy(gameObject);
+    }
+}
