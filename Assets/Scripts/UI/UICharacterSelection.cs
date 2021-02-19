9a6c9c3778cc261ddf92d5cd23f79d8ccade48cd
IkerNoya <ikernoya4@gmail.com>
2021-02-19 17:44:05 -0300

    Player can now change the color of the selected character

--- Assets/Scripts/UI/UICharacterSelection.cs
+++ Assets/Scripts/UI/UICharacterSelection.cs
@@ -7,10 +7,18 @@ using UnityEngine.UI;
 public class UICharacterSelection : MonoBehaviour
 {
     [SerializeField] GameObject ContinueButton;
+    [Space]
     [SerializeField] Image AvatarPlayer1;
     [SerializeField] Image AvatarPlayer2;
+    [Space]
     [SerializeField] Animator avatar1Controller;
     [SerializeField] Animator avatar2Controller;
+    [Space]
+    [SerializeField] GameObject colorSelectionLeft;
+    [SerializeField] GameObject leftWhiteButton;
+    [Space]
+    [SerializeField] GameObject colorSelectionRight;
+    [SerializeField] GameObject rightWhiteButton;
     public enum Player
     {
         player1, player2
@@ -32,6 +40,9 @@ public class UICharacterSelection : MonoBehaviour
 
         data = DataManager.instance;
 
+        colorSelectionLeft.SetActive(false);
+        colorSelectionRight.SetActive(false);
+
         EventSystem.current.SetSelectedGameObject(ContinueButton);
     }
     void Update()
@@ -50,31 +61,86 @@ public class UICharacterSelection : MonoBehaviour
     {
         if (player == Player.player1)
         {
-            avatar1Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/NovaAvatar") as RuntimeAnimatorController;
             data.player1Choice.playerSelection = DataManager.PlayerSelection.Nova;
-            AvatarPlayer1.color = visible;
+            colorSelectionLeft.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(leftWhiteButton);
         }
         else
         {
-            avatar2Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/NovaAvatar") as RuntimeAnimatorController;
             data.player2Choice.playerSelection = DataManager.PlayerSelection.Nova;
-            AvatarPlayer2.color = visible;
+            colorSelectionRight.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(rightWhiteButton);
         }
-        if (player == 0)
-            player++;
     }
     public void OnClickCyber()
     {
         if (player == Player.player1)
         {
             data.player1Choice.playerSelection = DataManager.PlayerSelection.CyberBunny;
+            colorSelectionLeft.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(leftWhiteButton);
         }
         else
         {
             data.player2Choice.playerSelection = DataManager.PlayerSelection.CyberBunny;
+            colorSelectionRight.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(rightWhiteButton);
         }
-        if (player == 0)
+    }
+    public void OnClickWhite(GameObject colors)
+    {
+        if(player==Player.player1)
+        {
+            data.player1Choice.tint = DataManager.Tint.white;
+            colors.SetActive(false);
+        }
+        else
+        {
+            data.player2Choice.tint = DataManager.Tint.white;
+            colors.SetActive(false);
+        }
+        if (player <= 0)
+        {
             player++;
+        }
+        EventSystem.current.SetSelectedGameObject(ContinueButton);
+    }
+    public void OnClickRed(GameObject colors)
+    {
+        if (player == Player.player1)
+        {
+            data.player1Choice.tint = DataManager.Tint.red;
+            colors.SetActive(false);
+        }
+        else
+        {
+            data.player2Choice.tint = DataManager.Tint.red;
+            colors.SetActive(false);
+        }
+        if (player <= 0)
+        {
+            player++;
+        }
+        EventSystem.current.SetSelectedGameObject(ContinueButton);
+
+    }
+    public void OnClickBlue(GameObject colors)
+    {
+        if (player == Player.player1)
+        {
+            data.player1Choice.tint = DataManager.Tint.blue;
+            colors.SetActive(false);
+        }
+        else
+        {
+            data.player2Choice.tint = DataManager.Tint.blue;
+            colors.SetActive(false);
+        }
+        if (player <= 0)
+        {
+            player++;
+        }
+        EventSystem.current.SetSelectedGameObject(ContinueButton);
     }
     public void OnHoverNova()
     {
@@ -92,7 +158,7 @@ public class UICharacterSelection : MonoBehaviour
     public void OnHoverCyberBunny()
     {
         if (player == Player.player1)
-        {
+        {   
             avatar1Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/CyberAvatar") as RuntimeAnimatorController;
             AvatarPlayer1.color = visible;
         }
@@ -100,6 +166,39 @@ public class UICharacterSelection : MonoBehaviour
         {
             avatar2Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/CyberAvatar") as RuntimeAnimatorController;
             AvatarPlayer2.color = visible;
+        }
+    }
+    public void OnHoverWhite()
+    {
+        if(player == Player.player1)
+        {
+            AvatarPlayer1.color = Color.white;
+        }
+        else
+        {
+            AvatarPlayer2.color = Color.white;
+        }
+    }
+    public void OnHoverRed()
+    {
+        if (player == Player.player1)
+        {
+            AvatarPlayer1.color = Color.red;
+        }
+        else
+        {
+            AvatarPlayer2.color = Color.red;
+        }
+    }
+    public void OnHoveBlue()
+    {
+        if (player == Player.player1)
+        {
+            AvatarPlayer1.color = Color.blue;
+        }
+        else
+        {
+            AvatarPlayer2.color = Color.blue;
         }
     }
 
