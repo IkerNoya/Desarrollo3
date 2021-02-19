b7f7b4a52a71156bdee4cdb5cdffcc893979fc96
IkerNoya <ikernoya4@gmail.com>
2021-02-18 21:29:25 -0300

    Added animated sprites to show player selection

--- Assets/Scripts/UI/UICharacterSelection.cs
+++ Assets/Scripts/UI/UICharacterSelection.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UICharacterSelection : MonoBehaviour
 {
     [SerializeField] GameObject ContinueButton;
+    [SerializeField] Image AvatarPlayer1;
+    [SerializeField] Image AvatarPlayer2;
+    [SerializeField] Animator avatar1Controller;
+    [SerializeField] Animator avatar2Controller;
+    
+    Color visible;
+    Color invisible;
+    public enum PlayerSelection
+    {
+        player1, player2
+    }
+    public PlayerSelection playerSelection;
     void Start()
     {
+        visible = AvatarPlayer1.color;
+        invisible = AvatarPlayer1.color;
+        invisible.a = 0;
+
+        AvatarPlayer1.color = invisible;
+        AvatarPlayer2.color = invisible;
         EventSystem.current.SetSelectedGameObject(ContinueButton);
     }
     void Update()
@@ -21,5 +40,36 @@ public class UICharacterSelection : MonoBehaviour
             EventSystem.current.SetSelectedGameObject(EventSystem.current.currentSelectedGameObject);
         }
     }
+    public void OnClickNova()
+    {
+        if (playerSelection == PlayerSelection.player1)
+        {
+            avatar1Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/NovaAvatar") as RuntimeAnimatorController;
+            AvatarPlayer1.color = visible;
+        }
+        else
+        {
+            avatar2Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/NovaAvatar") as RuntimeAnimatorController;
+            AvatarPlayer2.color = visible;
+        }
+        if(playerSelection==0)
+            playerSelection++;
+    }
+    public void OnClickCyber()
+    {
+        if (playerSelection == PlayerSelection.player1)
+        {
+            avatar1Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/CyberAvatar") as RuntimeAnimatorController;
+            AvatarPlayer1.color = visible;
+        }
+        else
+        {
+            avatar2Controller.runtimeAnimatorController = Resources.Load("Animations/UIAvatars/AnimationController/CyberAvatar") as RuntimeAnimatorController;
+            AvatarPlayer2.color = visible;
+        }
+        if (playerSelection == 0)
+            playerSelection++;
+    }
+
 
 }
