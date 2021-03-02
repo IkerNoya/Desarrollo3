db6244c9de9c9986ea63afcb5efcce08e2890360
IkerNoya <ikernoya4@gmail.com>
2021-03-02 10:44:47 -0300

    Imported new art assets

--- Assets/Scripts/Controllers/HabilityController.cs
+++ Assets/Scripts/Controllers/HabilityController.cs
@@ -8,19 +8,60 @@ public class HabilityController : MonoBehaviour
     {
         healing, chargeAttack, parry
     }
+    PlayerController player;
     public Hability hability;
+
     [SerializeField] KeyCode habilityKeyKM;
     [SerializeField] KeyCode habilityKeyJoystick;
+    [Space]
+    [SerializeField] float healingSpeed;
+    [SerializeField] float chargeSpeed;
+    [Space]
+    [SerializeField] float chargeCooldown;
+    [SerializeField] float parryCooldown;
+    [Space]
+
+    float timer;
     void Start()
     {
-        
+        player = GetComponentInParent<PlayerController>();
     }
 
     void Update()
     {
-        if(Input.GetKey(habilityKeyKM) || Input.GetKey(habilityKeyJoystick))
+        switch (hability)
         {
-            Debug.Log("Activate Hability");
+            case Hability.healing:
+                if (Input.GetKey(habilityKeyKM) || Input.GetKey(habilityKeyJoystick))
+                {
+                    player.hp += healingSpeed * Time.deltaTime;
+                }
+                break;
+            case Hability.chargeAttack:
+                if (timer <= 0)
+                {
+                    if (Input.GetKey(habilityKeyKM) || Input.GetKey(habilityKeyJoystick))
+                    {
+                        //chargeAttack + animation
+                    }
+                    else if(Input.GetKeyUp(habilityKeyKM) || Input.GetKeyUp(habilityKeyJoystick))
+                    {
+                        //attack + animation
+                        timer = chargeCooldown;
+                    }
+                }
+                break;
+            case Hability.parry:
+                if (timer <= 0)
+                {
+                    if (Input.GetKeyDown(habilityKeyKM) || Input.GetKeyDown(habilityKeyJoystick))
+                    {
+                        //parry + animation
+                        timer = parryCooldown;
+                    }
+                }
+                break;
         }
+        timer -= Time.deltaTime;
     }
 }
